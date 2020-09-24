0190cbcc57517849bad7f621b255b0de92c4f8d8
leonardovito <leonardo.vito@unicam.it>
2020-09-24 17:02:44 +0200

    New node

---
+++ Create_Dot_From_Fasta/Create_Dot_From_Fasta.js
@@ -0,0 +1,34 @@
+const { allowedNodeEnvironmentFlags, stdout } = require('process');
+
+module.exports = function(RED) {
+    function CreateDotFromFasta(config) {
+        RED.nodes.createNode(this,config);
+        var node = this;
+
+        this.fasta = config.fasta;
+        this.pathfolder = config.pathfolder;
+        this.outputname = config.outputname;
+
+        let command = 'RNAfold <' + node.fasta + ' > ' + node.pathfolder + '/' + node.outputname;
+
+        node.on('input', function(msg) {
+            if(node.fasta==null || node.fasta=='') {
+                command = 'RNAfold <' + msg.payload.fasta + ' > ' + msg.payload.pathfolder + '/' + msg.payload.outputname;
+            }
+            console.log(command)
+            const exec = require('child_process').exec;
+            const childPorcess = exec(command, function(err, stdout, stderr) {
+                if (err) {
+                    console.log(err);
+                    msg.payload.stdout = "Error";
+                    node.send(msg);
+                }
+                else{
+                    msg.payload.stdout = stdout;
+                    node.send(msg);
+                }
+            })
+        });
+    }
+    RED.nodes.registerType("create_dot_from_fasta",CreateDotFromFasta);
+}
